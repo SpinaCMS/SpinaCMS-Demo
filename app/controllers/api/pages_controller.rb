8ab1249b77e5d3f3bfb0d4d3c5b200de720c15d3
Bram Jetten <mail@bramjetten.nl>
2020-03-18 14:14:37 +0100

    Pages

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,6 +1,11 @@
 module Api
   class PagesController < ApplicationController
 
+    def index
+      @pages = Spina::Page.all
+      render json: @pages.map(&:to_json).to_json
+    end
+
     def show
       @page = Spina::Page.find(params[:id])
       render json: @page.to_json
